288d45a46ffce0c0b24b6a10a726784cbf796105
gtursi07 <guitursi@gmail.com>
2025-10-14 09:46:41 -0300

    Adicionado envio para a tela do técnico provisório.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,40 +5,37 @@ function logincheck() {
 
     const correctUsername = 'admin';
     const correctPassword = 'password123';
+    const correctTecnico = 'tecnico'
+
 
     if (username === correctUsername && password === correctPassword) {
         errorMessage.style.display = 'none';
         window.location.href = '/pages/index.html';
+    } else if (username === correctTecnico && password === correctPassword) {
+        errorMessage.style.display = 'none'
+        window.location.href = '/pages/pages_tecnico/menu_tecnico.html'
     } else {
         errorMessage.style.display = 'block';
     }
 }
-
-// 1. Função para alternar entre tema claro e escuro
+// função dark mode
 function toggleTheme() {
     const body = document.body;
     body.classList.toggle('dark-mode');
 
-    // Salvar a preferência do usuário no localStorage
     if (body.classList.contains('dark-mode')) {
-        // Se ativou o modo escuro
         localStorage.setItem('theme', 'dark');
     } else {
-        // Se desativou (voltou para o modo claro)
         localStorage.setItem('theme', 'light');
     }
 }
 
-// 2. Aplicar o tema salvo ao carregar a página
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Tenta obter a preferência salva
     const savedTheme = localStorage.getItem('theme');
 
-    // Se a preferência for 'dark', aplica a classe 'dark-mode'
     if (savedTheme === 'dark') {
         document.body.classList.add('dark-mode');
     } 
-    // Opcional: Se a preferência for 'light' ou não existir, garante que a classe 'dark-mode' não esteja lá
     else if (savedTheme === 'light') {
         document.body.classList.remove('dark-mode');
     }
